02e27324e662296a15cff692eee3117377c79aef
jcw1031 <jcw001031@gmail.com>
2024-10-29 17:20:08 +0900

    feature: 빠른 팟 생성 API 핸들러 구현
    * POST /api/v2/parties 요청 처리

--- core/core-api/src/main/java/com/woopaca/taximate/core/api/party/controller/InstantlyPartyController.java
+++ core/core-api/src/main/java/com/woopaca/taximate/core/api/party/controller/InstantlyPartyController.java
@@ -2,11 +2,19 @@ package com.woopaca.taximate.core.api.party.controller;
 
 import com.woopaca.taximate.core.api.common.model.ApiResults;
 import com.woopaca.taximate.core.api.common.model.ApiResults.ApiResponse;
+import com.woopaca.taximate.core.api.party.dto.request.CreateInstantlyPartyRequest;
+import com.woopaca.taximate.core.api.party.dto.response.CreatePartyResponse;
 import com.woopaca.taximate.core.api.party.dto.response.InstantlyPartiesResponse;
 import com.woopaca.taximate.core.domain.party.InstantlyParties;
+import com.woopaca.taximate.core.domain.party.Party;
 import com.woopaca.taximate.core.domain.party.service.PartyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -27,6 +35,15 @@ public class InstantlyPartyController {
         List<InstantlyPartiesResponse> response = instantlyParties.stream()
                 .map(party -> InstantlyPartiesResponse.of(party, instantlyParties.getAuthenticatedUser()))
                 .toList();
+        return ApiResults.success(response);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public ApiResponse<CreatePartyResponse> createParty(@Validated @RequestBody CreateInstantlyPartyRequest request) {
+        Party newParty = request.toDomain();
+        Long partyId = partyService.createInstantlyParty(newParty);
+        CreatePartyResponse response = new CreatePartyResponse(partyId);
         return ApiResults.success(response);
     }
 }
