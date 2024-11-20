0aef5eadc9c5b5fe4ab52b66a856f1659cb7122a
jcw1031 <jcw001031@gmail.com>
2024-11-20 23:47:54 +0900

    fix: 길찾기 결과가 없는 경우 처리
    * 경로는 빈 리스트, 예상 비용과 시간은 0으로 반환

--- core/core-api/src/main/java/com/woopaca/taximate/core/api/taxi/api/KakaoDirections.java
+++ core/core-api/src/main/java/com/woopaca/taximate/core/api/taxi/api/KakaoDirections.java
@@ -1,30 +1,42 @@
 package com.woopaca.taximate.core.api.taxi.api;
 
 import com.woopaca.taximate.core.domain.party.model.Coordinate;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
 public record KakaoDirections(List<Route> routes) {
 
     public List<Coordinate> route() {
-        return routes.get(0)
-                .sections.get(0)
+        List<Section> sections = routes.get(0)
+                .sections;
+        if (CollectionUtils.isEmpty(sections)) {
+            return List.of();
+        }
+
+        return sections.get(0)
                 .guides.stream()
-                .map(guide -> Coordinate.of(guide.y, guide.x))
+                .map(guide -> new Coordinate(guide.x, guide.y))
                 .toList();
     }
 
     public int fare() {
-        return routes.get(0)
-                .summary
-                .fare
+        Summary summary = routes.get(0)
+                .summary;
+        if (summary == null) {
+            return 0;
+        }
+        return summary.fare
                 .taxi;
     }
 
     public int duration() {
-        return routes.get(0)
-                .summary
-                .duration;
+        Summary summary = routes.get(0)
+                .summary;
+        if (summary == null) {
+            return 0;
+        }
+        return summary.duration;
     }
 
     record Route(Summary summary, List<Section> sections) {
